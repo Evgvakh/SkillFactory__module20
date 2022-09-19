eb9eafcc9190839ef741ff22cff902d2c6ee9aa3
Evgvakh <evgvakh@gmail.com>
2022-09-19 21:56:23 +0200

    second

--- task6/js/script.js
+++ task6/js/script.js
@@ -6,25 +6,8 @@ const button = document.querySelector('button');
 
 const divMain = document.querySelector('.main-container');
 
-function userRequest(target) {
-    return fetch (target)
-        .then((response) => { return response.json();})
-        .then((result) => { return result;});
-}
-
 window.onload = function() {
-    const storageItem = localStorage.getItem('img0');
-    console.log(storageItem);
-    if (storageItem) {
-        divMain.innerHTML = '';
-        for (let n = 0; n < localStorage.length; n++) {
-            let divImage = document.createElement('div');
-            divMain.appendChild(divImage);
-            let image = document.createElement('img');
-            image.setAttribute("src", localStorage[`img${n}`]);
-            divImage.appendChild(image);
-        }
-    }
+    
 };
 
 
@@ -32,29 +15,58 @@ button.addEventListener('click', async () => {
     let pageInputValue = pageInput.value;
     let limitInputValue = limitInput.value;
     console.log(pageInputValue, limitInputValue);
+    
     let link = `https://picsum.photos/v2/list?page=${pageInputValue}&limit=${limitInputValue}`; 
+    
+    const storageItem = localStorage.getItem('img0');
+
+    const storageLength = localStorage.length;
+
+    if (storageItem) {
+        divMain.innerHTML = '';
+        for (let n = 0; n < localStorage.length; n++) {
+            let divImage = document.createElement('div');
+            divMain.appendChild(divImage);
+            let image = document.createElement('img');
+            image.setAttribute("src", localStorage[`img${n}`]);
+            divImage.appendChild(image);            
+        }        
+    } 
+
+    localStorage.clear();
+
+    function userRequest(target) {
+        return fetch (target)
+            .then((response) => { return response.json();})
+            .then((result) => { return result;});
+    }
     
     const images = [];
 
     let userRequestResult = await userRequest(link);
 
-    // console.log(userRequestResult[1].url);
-
     function getImage(arr) {
         for (let i in arr) {
-            images.push(arr[i].url);
-            localStorage.setItem(`img${i}`, arr[i].url);
+            images.push(arr[i].url);            
         }
     }
 
     getImage(userRequestResult);
 
-    console.log(images);
+    for (let i in images) {
+        let divImage = document.createElement('div');
+        divMain.appendChild(divImage);
+        let image = document.createElement('img');
+        image.setAttribute("src", images[i]);
+        divImage.appendChild(image);
+        localStorage.setItem(`img${i}`, images[i]);
+    }
+       
 
-    // localStorage.setItem()
+    console.log(images);
 
     console.log(userRequestResult);
 
-
-
-})
+    console.log(storageItem);
+    
+});
